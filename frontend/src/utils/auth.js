1a225d19be62e08db2120358a288b3322ca8699d
soft-dev0420 <future930420@gmail.com>
2025-06-25 21:34:34 -0300

    first commit

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDWk027Iw9OYw6iL8f89yd-D35L1oHWt6I",
-  authDomain: "fba-shipment-f283c.firebaseapp.com",
-  projectId: "fba-shipment-f283c",
-  storageBucket: "fba-shipment-f283c.firebasestorage.app",
-  messagingSenderId: "218223107550",
-  appId: "1:218223107550:web:a5f6b72a3603f459a5114f",
-  measurementId: "G-J42LW6N28Z",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
